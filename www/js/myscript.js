62febc2612b0329581d27d9f101d8a9b8f0d63d1
Afrim Azizi <albanianboy2@gmail.com>
2015-05-05 12:44:06 +0200

    ads and speed issues fix

--- www/js/myscript.js
+++ www/js/myscript.js
@@ -1,6 +1,44 @@
-// RID PAGE TRANSITIONS
-$(document).bind("mobileinit", function(){
-      $.mobile.defaultPageTransition = 'none';
-      $.mobile.defaultDialogTransition = 'none';
-      $.mobile.useFastClick = true;
-});
+/* =============================================================================================================================================== */
+//Default transition
+function tranzicionet() { 
+    $.mobile.defaultPageTransition = "none";
+	rekllama();
+}
+
+function rekllama(){
+	var admobid = {};
+	if( /(android)/i.test(navigator.userAgent) ) { 
+		admobid = { // for Android
+			banner: 'ca-app-pub-9114339752225416/7304638684',
+			interstitial: ''
+		};
+	} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
+		admobid = { // for iOS
+			banner: '',
+			interstitial: ''
+		};
+	} else {
+		admobid = { // for Windows Phone
+			banner: '',
+			interstitial: ''
+		};
+	}
+
+	function initApp() {
+		alert('rekllama u krijua');
+		if (AdMob) {
+			AdMob.createBanner({
+				adId : admobid.banner,
+				position : AdMob.AD_POSITION.BOTTOM_CENTER,
+				autoShow : true
+			});
+		}
+	}
+
+	 if(( /(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent) )) {
+            document.addEventListener('deviceready', initApp, false);
+        } else {
+            initApp();
+        }
+//document.addEventListener('deviceready', initApp, false);
+}
