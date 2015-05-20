16a089f317641d0bce5a4cda59668b2116eef850
Afrim Azizi <albanianboy2@gmail.com>
2015-05-20 15:45:16 +0200

    update

--- www/js/myscript.js
+++ www/js/myscript.js
@@ -2,10 +2,33 @@
 //Default transition
 function tranzicionet() { 
     $.mobile.defaultPageTransition = "none";
-	rekllama();
+	//rekllama();
+	//prepareInterstitial();
 }
 
-function rekllama(){
+//rekllamat 
+admob.initAdmob("ca-app-pub-9114339752225416/7304638684","ca-app-pub-9114339752225416/3546823081");
+document.addEventListener(admob.Event.onInterstitialReceive, this.onInterstitialReceive, false);
+function cacheInterstitial(){
+	admob.cacheInterstitial();
+}
+function showBanner(){
+	admob.showBanner(admob.BannerSize.BANNER, admob.Position.BOTTOM_APP);
+}
+
+function hideBanner(){
+	admob.hideBanner();
+}
+
+function showInterstitial(){
+	admob.isInterstitialReady(function(isReady){
+		if(isReady){
+			admob.showInterstitial();
+		}
+	});
+}
+
+/*function rekllama(){
 	var admobid = {};
 	if( /(android)/i.test(navigator.userAgent) ) { 
 		admobid = { // for Android
@@ -39,11 +62,15 @@ function rekllama(){
         } else {
             initApp();
         }
-		if(AdMob) AdMob.prepareInterstitial( {adId:admobid.interstitial, autoShow:false} );
+		
+}
+
+function prepareInterstitial() {
+	alert('preparation complete');
+       AdMob.prepareInterstitial({adId:admobid.interstitial, autoShow:false});
+}
 
 // show the interstitial later, e.g. at end of game level
 if(AdMob) AdMob.showInterstitial();
-}
-
 //document.addEventListener('deviceready', initApp, false);
-}
+}*/
