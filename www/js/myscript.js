edd09ef69667bf9a0f8387fffeb488ec1589e65f
Afrim Azizi <albanianboy2@gmail.com>
2015-05-27 19:43:15 +0200

    clean

--- www/js/myscript.js
+++ www/js/myscript.js
@@ -51,38 +51,3 @@ function hideBanner(){
 function showInterstitial(){
 	admob.showInterstitialAd();
 }
-
-/*function rekllama(){
-	var admobid = {};
-	if( /(android)/i.test(navigator.userAgent) ) { 
-		admobid = { // for Android
-			banner: 'ca-app-pub-9114339752225416/7304638684',
-			interstitial: 'ca-app-pub-9114339752225416/3546823081'
-		};
-	} else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
-		admobid = { // for iOS
-			banner: 'ca-app-pub-9114339752225416/7304638684',
-			interstitial: 'ca-app-pub-9114339752225416/3546823081'
-		};
-	} else {
-		admobid = { // for Windows Phone
-			banner: '',
-			interstitial: ''
-		};
-	}
-	function initApp() {
-		if (AdMob) {
-			AdMob.createBanner({
-				adId : admobid.banner,
-				position : AdMob.AD_POSITION.BOTTOM_CENTER,
-				autoShow : true
-			});
-		}
-	}
-	 if(( /(ipad|iphone|ipod|android|windows phone)/i.test(navigator.userAgent) )) {
-            document.addEventListener('deviceready', initApp, false);
-        } else {
-            initApp();
-        }
-		
-}*/
